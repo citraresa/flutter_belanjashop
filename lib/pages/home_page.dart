38fb9a8735ecfdd323a4af3fbbb4ebd77fbb9e18
citraresa <citraresa1@gmail.com>
2025-09-09 22:46:50 +0700

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,35 +1,101 @@
 import 'package:flutter/material.dart';
 import '../models/item.dart';
-import 'item_page.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   final List<Item> items = [
-    Item(name: "Sugar", price: 5000),
-    Item(name: "Salt", price: 2000),
+    Item(name: "Beras", price: 70000, photo: "assets/beras.jpeg", stock: 10, rating: 4.5),
+    Item(name: "Gula", price: 17000, photo: "assets/gula.jpeg", stock: 20, rating: 4.8),
+    Item(name: "Garamg", price: 8000, photo: "assets/garam.jpeg", stock: 30, rating: 4.1),
+    Item(name: "Telur", price: 30000, photo: "assets/telur.jpeg", stock: 25, rating: 4.7),
+   
   ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Shopping List")),
-      body: ListView.builder(
+      appBar: AppBar(title: const Text("Shopinglist")),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(8.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.75,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
+        ),
         itemCount: items.length,
         itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              // Navigasi ke halaman detail
-              Navigator.pushNamed(context, '/item', arguments: Item);
-            },
-            child: Card(
-              child: ListTile(
-                title: Text(items[index].name),
-                subtitle: Text(items[index].price.toString()),
+          final item = items[index];
+          return ItemCard(item: item);
+        },
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Nama: Citra Resa - NIM: [362458302019]',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final Item item;
+
+  const ItemCard({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, '/item', arguments: item);
+      },
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Hero(
+                tag: 'itemPhoto${item.name}', //hero 
+                child: Image.network(
+                  item.photo,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          );
-        },
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                item.name,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text("Rp ${item.price}"),
+            ),
+            Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+             child: Text("Stok: ${item.stock}"),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 8.0),
+              child: Row(
+                children: [
+                  const Icon(Icons.star, color: Colors.amber, size: 20),
+                  const SizedBox(width: 4),
+                  Text(item.rating.toString()),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
